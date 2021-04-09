420adfa8a77d69647f82b14001734e4ba2f73fd1
Neeklow - Niccolo Fioritti <1810Nick!>
2021-04-09 14:41:20 +0100

    Annotated and cleaned txt_to_vtk_converter

--- example files/txt_to_vtk_converter.py
+++ example files/txt_to_vtk_converter.py
@@ -11,35 +11,25 @@ TXT TO VTK FILE EXPORTER
 
 
 import re
-
 filename = 'exported_coord.txt'
-
 converted = 'converted.vtk'
-
-
 pointsList=[]
 
+#Get points from original.txt, clean it and move it to a list.
+#It can be neater if the original exported txt is a simple csv file.
 
 with open (filename) as file:
-    
-   
-    
     pointsCount =0
     for x in file:
         x=x.rstrip()
         x = re.sub('[(),]', '' , x)
-       
         pointsList.append(x)
-       
-  
-    
+
+
 with open(converted,'w') as c:
-    pointsCount=len(pointsList)   
- 
-    header =str('# vtk DataFile Version 3.0\nvtk output\nASCII\nDATASET POLYDATA\nPOINTS %d float\n' %pointsCount)
+    pointsCount=len(pointsList)
+    #the header definistion is a bit clunky. It works but really not elastic. Praying that the header does not need any change aside the point number
+    header =str('# vtk DataFile Version 3.0\nvtk output\nASCII\nDATASET POLYDATA\nPOINTS %d float\n' %pointsCount) 
     c.write (header)
     for point in pointsList:
         c.write(point +'\n')
-    
-   # print(pointsCount)
-   # file = re.sub('[()]', '' , file)
