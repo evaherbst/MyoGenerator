d75f76282fc5e1e1de3b4167edb03be187617dc2
Eva C. Herbst <31034865+evaherbst@users.noreply.github.com>
2021-04-20 09:05:34 +0200

    Update Python_Niva_Muscle_Analyzer.py
    reverted to original to fix index error, added centroid calculation

--- Python_Niva_Muscle_Analyzer.py
+++ Python_Niva_Muscle_Analyzer.py
@@ -21,73 +21,139 @@ def object_Recenter():
 
 
 #calculate muscle attachment 
-def get_attachment_area(obj):   
-  bpy.ops.object.transform_apply(location=False, rotation=True, scale=True)  #set scale and rotation = 1 to get correct volume values and apply other transforms, do not set location to 0 because we want to know global position
-  objName=obj.name 
-  me = obj.data #get mesh of object
-  me.name=objName
-  # Get a BMesh representation
-  bm = bmesh.new()# create an empty BMesh
-  bm.from_mesh(me)# fill it in from a Mesh
-  print(objName + " Area")
-  area = sum(f.calc_area() for f in bm.faces)
-  #bm.clear()
-  print(area)
-  return area
+def get_attachment_area(obj):
+    
+    bpy.ops.object.transform_apply(location=False, rotation=True, scale=True)  #set scale and rotation = 1 to get correct volume values and apply other transforms, do not set location to 0 because we want object origin set to geometry
+    objName=obj.name 
+    me = obj.data #get mesh of object
+    me.name=objName
+    # Get a BMesh representation
+    bm = bmesh.new()# create an empty BMesh
+    bm.from_mesh(me)# fill it in from a Mesh
+    print(objName + " Area")
+    area = sum(f.calc_area() for f in bm.faces)
+    #bm.clear()
+    print(area)
+    return area
+        
         
 def get_centroid(obj):
   centroid = obj.location
-  print(centroid)
+
 
 
 
 
 def measure_muscle_volume(obj):
-  bpy.ops.object.mode_set(mode = 'OBJECT')
-  bpy.ops.object.transform_apply(location = False, scale = True, rotation = True) #set scale and rotation = 1 to get correct volume values and apply other transforms, do not set location to 0 because we want to know global position
-  me = obj.data
-  # Get a BMesh representation
-  bm = bmesh.new() # create an empty BMesh
-  bm.from_mesh(me) # fill it in from a Mesh
-  # triangulate prior makes the difference
-  bmesh.ops.triangulate(bm, faces=bm.faces)
-  print("Volume")
-  volume = bm.calc_volume()
-  print(volume)
-  return volume
+    bpy.ops.object.transform_apply(location = False, scale = True, rotation = True) #set scale and rotation = 1 to get correct volume values and apply other transforms, do not set location to 0 because we want object origin set to geometry
+    objName=obj.name 
+    me = obj.data
+    # Get a BMesh representation
+    bm = bmesh.new() # create an empty BMesh
+    bm.from_mesh(me) # fill it in from a Mesh
+    # triangulate prior makes the difference
+    bmesh.ops.triangulate(bm, faces=bm.faces)
+    print("Volume")
+    volume = bm.calc_volume()
+    print(volume)
+    return volume
 
-#calculate the muscle length (= distance between the center of mass of the muscle origin and insertion)
+# calculate the muscle length (= distance between the center of mass of the muscle origin and insertion)
 def get_muscle_length(attachment_list):
-  l = []
-  if (isinstance(attachment_list,list)):
-    for item in attachment_list:
-        l.append(item.location)
-    length = sqrt( (l[0][0] - l[1][0])**2 + (l[0][1] - l[1][1])**2 + (l[0][2] - l[1][2])**2)
-    print("Muscle Length")
-    print(length)
-    return length
-  else:
-    print("attachment list needs to be a list")
-    return ("MISSING VALUE")
+    l = []
+    if (isinstance(attachment_list,list)):
+      for item in attachment_list:
+          l.append(item.location)
+      length = sqrt( (l[0][0] - l[1][0])**2 + (l[0][1] - l[1][1])**2 + (l[0][2] - l[1][2])**2)
+      print("Muscle Length")
+      print(length)
+      return length
+    else:
+      print("attachment list needs to be a list")
+      return ("MISSING VALUE")
    
+
+def main_loop():
+  bpy.ops.object.mode_set(mode = 'OBJECT')
+  bpy.ops.object.select_all(action='SELECT')
+  bpy.ops.object.transform_apply(location = False, scale = True, rotation = True) #set scale and rotation = 1 to get correct volume values and apply other transforms, 
+  # do not set location to 0 because we want origin centered based on geometry
+  #or add code to make sure all calculations are in global coordinates?
+  complete_Muscle_List = []
+  muscle_Data = [] #create a list to store muscle data
+  attachment_list = [] #create a list of origin and insertion objects
+  origin_area=0
+  insertion_area=0
+  muscle_volume=0
+  muscle_length=0
+  origin_centroid=0
+  insertion_centroid=0
+  for obj in bpy.context.selected_objects:
+    if obj.type == 'EMPTY':
+      muscle_name=obj.name
+      children = []
+      children = obj.children
+      for obj in children:
+        if "origin" in obj.name:
+          origin_area=get_attachment_area(obj) 
+          attachment_list.append(obj)
+          origin_centroid=obj.location
+        elif "insertion" in obj.name:
+          insertion_area=get_attachment_area(obj) 
+          attachment_list.append(obj)
+          insertion_centroid=obj.location
+        elif "volume" in obj.name:
+          muscle_volume=measure_muscle_volume(obj)
+        else:
+          print("Unproper naming of children. The following object will be ignored: "+obj.name)  
+      muscle_length=get_muscle_length(attachment_list)
+      muscle_Data = [muscle_name, origin_area, insertion_area, muscle_length, muscle_volume]
+      print(muscle_Data)
+      complete_Muscle_List.append((muscle_Data))
+      print(complete_Muscle_List)
+      export(complete_Muscle_List)
+ 
+
 def export(complete_Muscle_List):
   filepath = bpy.data.filepath
   main_path = os.path.dirname(filepath)
   print("writing to: " + main_path)
   outputFile = main_path + '\muscle metrics.csv' #save output to same folder as blender file
-  header = [['muscle_name', ' origin_area', ' origin_centroid', 'insertion_area', 'insertion_centroid', ' length', ' volume']]
+  header = [['muscle_name', ' origin_area', ' origin_centroid', ' insertion_area', ' insertion_centroid', ' length', ' volume']]
   with open(outputFile, "w", newline='') as f:
       writer = csv.writer(f)
       writer.writerows(header)
       writer.writerows(complete_Muscle_List)
 
+
+object_Recenter() 
+main_loop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def main_loop():
   bpy.ops.object.mode_set(mode = 'OBJECT')
   bpy.ops.object.select_all(action='SELECT')
-  bpy.ops.object.transform_apply(location = False, scale = True, rotation = True) #set scale and rotation = 1 to get correct volume values and apply other transforms, do not set location to 0 because we want to know global position
-  # NEED TO CHECK IF THE PARENTING GIVES WRONG COORDINATES
-  #size should be ok if parent empty is at origin w/ transforms set to 0 but need to check if rotations change anything
-  #checked by parenting boundary loop to origin area (with faces), parenting didn't change origin but maybe bc same origin - try with different meshes
+  bpy.ops.object.transform_apply(location = False, scale = True, rotation = True) #set scale and rotation = 1 to get correct volume values and apply other transforms, 
+  # do not set location to 0 because we want origin centered based on geometry
   #or add code to make sure all calculations are in global coordinates?
   complete_Muscle_List = []
   muscle_Data = [] #create a list to store muscle data
@@ -128,8 +194,8 @@ def main_loop():
         point1=origin_centroid
         point2=insertion_centroid
         length = math.sqrt((point2[0] - point1[0]) ** 2 + (point2[1] - point1[1]) ** 2 + (point2[2] - point1[2]) ** 2) 
-   # muscle_length=get_muscle_length(attachment_list)
-   muscle_Data = [muscle_name, origin_area, origin_centroid, insertion_area, insertion_centroid, muscle_length, muscle_volume] #has issues with muscle_nae
+    muscle_length=get_muscle_length(attachment_list)
+    muscle_Data = [muscle_name, origin_area, origin_centroid, insertion_area, insertion_centroid, muscle_length, muscle_volume] #has issues with muscle_nae
     print(muscle_Data)
     complete_Muscle_List.append((muscle_Data))
     print(complete_Muscle_List)
