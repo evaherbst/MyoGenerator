8f77a8fc53ec86e149af65e318634bf0b7eba675
Eva C. Herbst <31034865+evaherbst@users.noreply.github.com>
2021-06-01 17:53:34 +0100

    Update Blender_muscle_creator.py
    added more scripts and reorganized

--- Blender_muscle_creator.py
+++ Blender_muscle_creator.py
@@ -9,22 +9,96 @@ way to get all the muscle data or whether to just save as we go along for each m
 
 import bpy
 
-def make_muscle_empties():
-	#enter your list of muscles here
-	muscle_List = ["mPT", "mLPt", "mPPt","mPSTs", "mPSTp", "mAMEP", "mAMEM", "mAMES", "mAMP", "mDM"]
-	for i in muscle_List:
-		print(i)
-		o = bpy.data.objects.new(i, None)
-		bpy.context.scene.collection.objects.link( o )
-		o.empty_display_size = 2
-		o.empty_display_type = 'PLAIN_AXES'   
+def make_empty(Muscle):
+	o = bpy.data.objects.new(Muscle, None)
+	bpy.context.scene.collection.objects.link( o )
+	o.empty_display_size = 2
+	o.empty_display_type = 'PLAIN_AXES'   
 		 
 
 make_muscle_empties()
 
 
 
+def reorder_coords(obj):
+#to use for getting origin and insertion vertex numbers the same
+    
+#Do I need to deselect all here, and then make this object active? or is it enough to just have the obj argument?
+#Do I need to make the object active? or is it fine with the input argument? #Test!
+	bpy.ops.object.mode_set(mode = 'OBJECT') 
+	bpy.ops.object.select_all(action='DESELECT')
+	obj.select_set(True) #selects boundary
+	bpy.context.view_layer.objects.active = bpy.data.objects[obj.name] #sets boundary as active mesh
+	bpy.ops.object.mode_set(mode = 'EDIT')
+	bpy.context.tool_settings.mesh_select_mode = (True, False, False) #vertex select mode
+	bpy.ops.mesh.select_all(action='SELECT') #select all vertices
 
+	#somehow the above section and the below section run well separately but not if I run them together as one chunk..
+	me = bpy.context.object.data
+	bm = bmesh.from_edit_mesh(me)
+
+	# index of the start vertex
+	initial = bm.verts[0]
+
+	vert = initial
+	prev = None
+	for i in range(len(bm.verts)):
+	    print(vert.index, i)
+	    vert.index = i
+	    next = None
+	    adjacent = []
+	    for v in [e.other_vert(vert) for e in vert.link_edges]:
+	        if (v != prev and v != initial):
+	            next = v
+	    if next == None: break
+	    prev, vert = vert, next
+
+	bm.verts.sort()
+
+bmesh.update_edit_mesh(me)
+
+
+
+
+
+def create_boundary(obj): #this works well - makes boundary, parents to attachment area area
+
+
+
+	#Do I need to deselect all here, and then make this object active? or is it enough to just have the obj argument?
+	#Do I need to make the object active? or is it fine with the input argument? #Test!
+	name = obj.name
+
+	# keep track of objects in scene to later rename new objects
+	scn = bpy.context.scene
+	names = [ obj.name for obj in scn.objects]
+
+
+	bpy.ops.object.mode_set(mode = 'EDIT')
+	bpy.ops.mesh.select_all(action='SELECT')
+
+
+	#select outer loop, duplicate, separate
+	bpy.ops.mesh.region_to_loop()
+	bpy.ops.mesh.duplicate()
+	bpy.ops.mesh.separate(type='SELECTED')
+
+	bpy.ops.object.mode_set(mode = 'OBJECT')
+	bpy.ops.object.select_all(action='DESELECT') 
+
+
+new_objs = [ obj for obj in scn.objects if not obj.name in names]
+
+#rename new object and select and make active
+for obj in new_objs:
+    obj.name = name + " boundary"
+    obj.data.name = obj.name #set mesh name to object name
+    obj.select_set(True)
+    bpy.context.view_layer.objects.active = bpy.data.objects[name]
+    bpy.data.objects[name].select_set(True)
+    bpy.ops.object.parent_set(keep_transform=True) #parents new loop to the attachment area - need to double check that the transforms are all global 
+    bpy.context.view_layer.objects.active = bpy.data.objects[name + " boundary"]
+return obj
 
 
 
@@ -40,9 +114,11 @@ import bmesh
 """ORIGIN CREATION"""
 
 """user specifies muscle name"""
-Muscle = "mPSTp"
+Muscle = "mPSTp" 
 
 bpy.ops.object.mode_set(mode = 'OBJECT')
+
+make_empty(Muscle)
 
 
 """prompt user to select bone on which to draw origin - needs to be meshed nicely and if several bones they need to be one object""" 
@@ -164,32 +240,47 @@ bpy.context.view_layer.objects.active = bpy.data.objects[Muscle + " insertion"]
 
 
 
-"""MUSCLE VOLUME CREATION"""
+"""MUSCLE VOLUME CREATION - IN PROGRESS"""
 
 
-"""add modified tube tool or add shape key method to make muscle volume
+#create boundary for origin and insertion, duplicate, save serapately 
+origin = bpy.data.objects[Muscle + " origin"]
+insertion = bpy.data.objects[Muscle + " insertion"]
+create_boundary(origin)
+create_boundary(insertion)
+
+    if "boundary" in obj.name 
+        reorder_coords(obj) 
+
+### match vertex counts of origin and insertion here:
+#- count vertices in origin and insertion
+#calculate how many subdivisions x are needed (x = difference in vertices between 2 objects)
+#take object that has less vertice, and select every n pairs of vertices and subdivide (until there are x number of subdivisions performed
 
 
-if we use tube tool, need to add conver origins and insertions into single face with:
-create_boundary script (see exporter_for_MuscleDecomposition) and this:
 
-
+#If we need to fill edge loop for face normal, duplicate boundary and use the following
 bpy.ops.object.mode_set(mode = 'EDIT')
 bpy.context.tool_settings.mesh_select_mode = (False, True, False) #edge select mode
 bpy.ops.mesh.select_all(action='SELECT')
 
-fill edge loop to make 1 face
+#fill edge loop to make 1 face
 bpy.ops.mesh.edge_face_add() 
 
-- need to decide if we want to export centroid coords before simplifying to single face
-- try to figure out how to calculate curve length for muscle length"""
+
+"""BEZIER CURVE"""
+
+bpy.ops.curve.primitive_bezier_curve_add(radius=1, enter_editmode=False, align='WORLD', location=(0, 0, 0), scale=(1, 1, 1))
 
 
+bpy.context.view_layer.objects.active = bpy.data.objects["BezierCurve"]
+curve = bpy.context.object
+bez_points = curve.data.splines[0].bezier_points
+bez_points[0].co = (3,3,3) #replace with origin coord
+bez_points[1].co = (6,6,6) #replace with insertion coord
+
+#se geometric origin of Bezier curve to the muscle origin attachment centroid, and also set geometric origin of array mesh the muscle origin attachment centroid
 
 
-# bpy.ops.curve.primitive_bezier_circle_add()    #for blending
-# obj = bpy.context.object
-# bpy.ops.curve.primitive_bezier_curve_add()     # a curve, your endpoints for example
-# bpy.context.object.data.bevel_object = bpy.data.objects[obj.name] #a cylinder
 
 
