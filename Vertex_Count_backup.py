28f1fbe49f6b46d60aa08a716f6193a83fd35560
Eva C. Herbst <31034865+evaherbst@users.noreply.github.com>
2021-06-03 17:10:00 +0100

    Update Vertex_Count_backup.py
    trying to solve issues and failing - but reorder_coords works now!

--- Vertex_Count_backup.py
+++ Vertex_Count_backup.py
@@ -1,52 +1,128 @@
+import bpy
+import bmesh
+import math
 
-#maybe a for loop to get all objects with "Muscle" in name, then for both origin boundary and insertion, do the following:
+def change_vertex_number(originCount,insertionCount):
 
+  print("OriginCount = ", originCount," InsCount =", insertionCount)
+  
+  vertexDiff = 2
+  #vertexDiff=abs(originCount-insertionCount)
+  print ("vert Diff", vertexDiff)
+  counter = 0
 
-
-
-
-def reorder_coords(obj):
-#to use for getting origin and insertion vertex numbers the same
+  if(originCount>insertionCount):
     
-#make sure correct object is active!
-
-	bpy.ops.object.mode_set(mode = 'OBJECT') 
-	bpy.ops.object.select_all(action='DESELECT')
-	obj.select_set(True) #selects boundary
-	bpy.context.view_layer.objects.active = bpy.data.objects[obj.name] #sets boundary as active mesh
-	bpy.ops.object.mode_set(mode = 'EDIT')
-	bpy.context.tool_settings.mesh_select_mode = (True, False, False) #vertex select mode
-	bpy.ops.mesh.select_all(action='SELECT') #select all vertices
-
-	#somehow the above section and the below section run well separately but not if I run them together as one chunk..
-	me = bpy.context.object.data
-	bm = bmesh.from_edit_mesh(me)
-
-	# index of the start vertex
-	initial = bm.verts[0]
-
-	vert = initial
-	prev = None
-	for i in range(len(bm.verts)):
-	    print(vert.index, i)
-	    vert.index = i
-	    next = None
-	    adjacent = []
-	    for v in [e.other_vert(vert) for e in vert.link_edges]:
-	        if (v != prev and v != initial):
-	            next = v
-	    if next == None: break
-	    prev, vert = vert, next
-
-	bm.verts.sort()
-
-bmesh.update_edit_mesh(me)
+    print("ORIGIN > INSERTION")
+    #need to divide "random" edges vertexDiff times
+    # insertionCount-1 / vertexDiff = how many times to jump
+    #check if number vertexDiff < vertexCount
+    #pick indexes to subdivide (int(vertexCount/vertexDiff))
+    # for i in
+    
+    increment = math.floor((insertionCount)/vertexDiff) #rounds down to nearest whole number increment
+    
+    insertion_boundary_obj.select_set(True)
+    bpy.ops.object.mode_set(mode = 'EDIT')
+    bpy.context.tool_settings.mesh_select_mode = (False, True, False) #edge select mode
+    
+    for x in range(0, insertionCount-1, increment):
 
 
+      if(counter< vertexDiff):
+        counter+=1 
+        insertion_boundary_obj.select_set(True)
+        #edgeList [ x ] . subdivide
+        bpy.ops.mesh.select_all(action='DESELECT')
+        bpy.ops.object.mode_set(mode = 'OBJECT')
+        obj.data.edges[x].select = True
+        bpy.ops.object.mode_set(mode = 'EDIT') 
+        bpy.ops.mesh.subdivide()
+
+    #  else:
+    #    break
+  
+  if (originCount < insertionCount):
+    print("ORIGIN < INSERTION")
+    increment = math.floor((originCount)/vertexDiff) #rounds down 
+
+    origin_boundary_obj.select_set(True)
+    bpy.ops.object.mode_set(mode = 'EDIT')
+    bpy.context.tool_settings.mesh_select_mode = (False, True, False) #edge select mode
+    
+    for x in range(0, originCount-1, increment):
+
+      if(counter< vertexDiff):
+        counter+=1        
+        bpy.ops.mesh.select_all(action='DESELECT')
+        bpy.ops.object.mode_set(mode = 'OBJECT')
+        obj.data.edges[x].select = True
+        bpy.ops.object.mode_set(mode = 'EDIT') 
+        bpy.ops.mesh.subdivide()
+
+    #  else:
+    #    break
+
+  if (originCount == insertionCount):
+
+    print('wow, that was lucky')
 
 
-#then count vertices, find object with fewer vertices, add vertices
+def reorder_coords(obj): 
+
+    bpy.ops.object.mode_set(mode = 'OBJECT') 
+    bpy.ops.object.select_all(action='DESELECT')
+    obj.select_set(True) #selects boundary
+    bpy.context.view_layer.objects.active = bpy.data.objects[obj.name] #sets boundary as active mesh
+    bpy.ops.object.mode_set(mode = 'EDIT')
+    bpy.context.tool_settings.mesh_select_mode = (True, False, False) #vertex select mode
+    bpy.ops.mesh.select_all(action='SELECT') #select all vertices
+    me = bpy.context.object.data
+    bm = bmesh.from_edit_mesh(me)
+    if hasattr(bm.verts,"ensure_lookup_table"):
+        bm.verts.ensure_lookup_table()
+    # index of the start vertex
+    initial = bm.verts[0]
+    vert = initial
+    prev = None
+    for i in range(len(bm.verts)):
+        print(vert.index, i)
+        vert.index = i
+        next = None
+        adjacent = []
+        for v in [e.other_vert(vert) for e in vert.link_edges]:
+            if (v != prev and v != initial):
+                next = v
+        if next == None: break
+        prev, vert = vert, next
+    bm.verts.sort()
+    bmesh.update_edit_mesh(me)
+    return len(bm.verts)
+
+# reorder_coords(bpy.context.view_layer.objects.active)
 
 
 
-bpy.ops.mesh.subdivide()
+
+Muscle = "Muscle"
+
+bpy.ops.object.mode_set(mode = 'OBJECT') 
+bpy.ops.object.select_all(action='SELECT')
+
+for obj in bpy.context.selected_objects:
+  if Muscle in obj.name and "boundary" in obj.name:
+    print(obj)
+    if "origin" in obj.name:
+      origin_boundary_obj = obj
+      originVertexCount = reorder_coords(obj)
+      print("reordering origin")
+    if "insertion" in obj.name:
+      insertionVertexCount = reorder_coords(obj)
+      insertion_boundary_obj = obj
+      print("reordering insertion")
+
+print(insertionVertexCount, " ORIGINVERTEXCOUNT")
+
+change_vertex_number(originVertexCount,insertionVertexCount)
+
+
