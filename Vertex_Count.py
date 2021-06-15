a812e888489c9a189e17e47e35c9216262609770
Eva C. Herbst <31034865+evaherbst@users.noreply.github.com>
2021-06-15 11:48:15 +0100

    Update Vertex_Count.py
    fixed!!!!

--- Vertex_Count.py
+++ Vertex_Count.py
@@ -19,95 +19,87 @@ def change_vertex_number(originCount,insertionCount,origin_boundary_obj,insertio
     bpy.ops.object.mode_set(mode = 'EDIT')
     bpy.context.tool_settings.mesh_select_mode = (False, True, False) #edge select mode
     obj = origin_boundary_obj
-    print(obj.name)
+    bpy.ops.mesh.select_all(action='DESELECT')
+    bpy.ops.object.mode_set(mode = 'OBJECT')
     for x in range(0, originCount-1, increment):
       if(counter< vertexDiff):
-        counter+=1        
-        bpy.ops.mesh.select_all(action='DESELECT')
+        counter+=1
         bpy.ops.object.mode_set(mode = 'OBJECT')
+        for edge in obj.data.edges:
+          edge.select = False
         obj.data.edges[x].select = True
-        bpy.ops.object.mode_set(mode = 'EDIT') 
+        bpy.ops.object.mode_set(mode = 'EDIT')
         bpy.ops.mesh.subdivide()
+        print(counter)
         print("subdivision successful")
-  elif (originCount > insertionCount): #Should this be elif?? 
+  elif (originCount > insertionCount):
     print("ORIGIN > INSERTION")
     increment = math.floor((insertionCount)/vertexDiff) #rounds down to nearest whole number increment
     print(increment)
     insertion_boundary_obj.select_set(True)
     bpy.ops.object.mode_set(mode = 'EDIT')
     bpy.context.tool_settings.mesh_select_mode = (False, True, False) #edge select mode
-    for x in range(0, insertionCount-1, increment):
-      if(counter < vertexDiff):
-        counter+=1 
-        insertion_boundary_obj.select_set(True)
-        bpy.ops.mesh.select_all(action='DESELECT')
+    obj = insertion_boundary_obj
+    bpy.ops.mesh.select_all(action='DESELECT')
+    bpy.ops.object.mode_set(mode = 'OBJECT')
+    for x in range(0, originCount-1, increment):
+      if(counter< vertexDiff):
+        counter+=1
         bpy.ops.object.mode_set(mode = 'OBJECT')
+        for edge in obj.data.edges:
+          edge.select = False
         obj.data.edges[x].select = True
-        bpy.ops.object.mode_set(mode = 'EDIT') 
+        bpy.ops.object.mode_set(mode = 'EDIT')
         bpy.ops.mesh.subdivide()
+        print(counter)
+        print("subdivision successful")
   elif (originCount == insertionCount):
     print('wow, that was lucky')
 
-def reorder_coords(obj): 
-    # global originCount
-    # global insertionCount
-    bpy.ops.object.mode_set(mode = 'OBJECT') 
-    bpy.ops.object.select_all(action='DESELECT')
-    obj.select_set(True) #selects boundary
-    bpy.context.view_layer.objects.active = bpy.data.objects[obj.name] #sets boundary as active mesh
-    bpy.ops.object.mode_set(mode = 'EDIT')
-    bpy.context.tool_settings.mesh_select_mode = (True, False, False) #vertex select mode
-    bpy.ops.mesh.select_all(action='SELECT') #select all vertices
-    me = bpy.context.object.data
-    bm = bmesh.from_edit_mesh(me)
-    if hasattr(bm.verts,"ensure_lookup_table"):
-        bm.verts.ensure_lookup_table()
-    initial = bm.verts[0]
-    vert = initial
-    prev = None
-    for i in range(len(bm.verts)):
-        print(vert.index, i)
-        vert.index = i
-        next = None
-        adjacent = []
-        for v in [e.other_vert(vert) for e in vert.link_edges]:
-            if (v != prev and v != initial):
-                next = v
-        if next == None: break
-        prev, vert = vert, next
-    bm.verts.sort()
-    bmesh.update_edit_mesh(me)
-    # if "origin" in obj.name:
-    #     originCount = len(bm.verts)
-    #     print (obj.name)
-    # if "insertion" in obj.name:
-    #     insertionCount = len(bm.verts)
-    #     print (obj.name)
-    return len(bm.verts)
+def reorder_coords(obj):
+  bpy.ops.object.mode_set(mode = 'OBJECT')
+  bpy.ops.object.select_all(action='DESELECT')
+  obj.select_set(True) #selects boundary
+  bpy.context.view_layer.objects.active = bpy.data.objects[obj.name] #sets boundary as active mesh
+  bpy.ops.object.mode_set(mode = 'EDIT')
+  bpy.context.tool_settings.mesh_select_mode = (True, False, False) #vertex select mode
+  bpy.ops.mesh.select_all(action='SELECT') #select all vertices
+  me = bpy.context.object.data
+  bm = bmesh.from_edit_mesh(me)
+  if hasattr(bm.verts,"ensure_lookup_table"):
+      bm.verts.ensure_lookup_table()
+  initial = bm.verts[0]
+  vert = initial
+  prev = None
+  for i in range(len(bm.verts)):
+      print(vert.index, i)
+      vert.index = i
+      next = None
+      adjacent = []
+      for v in [e.other_vert(vert) for e in vert.link_edges]:
+          if (v != prev and v != initial):
+              next = v
+      if next == None: break
+      prev, vert = vert, next
+  bm.verts.sort()
+  bmesh.update_edit_mesh(me)
+  return len(bm.verts)
 
-# origin_boundary_obj = bpy.context.view_layer.objects.active
-# insertion_boundary_obj = bpy.context.view_layer.objects.active
-# obj = bpy.context.view_layer.objects.active
-
-Muscle = "mDM"
-# originCount = 0
-# insertionCount = 0
-# origin_boundary_obj = 0
-# insertion_boundary_obj = 0
-bpy.ops.object.mode_set(mode = 'OBJECT') 
+Muscle = "mDm"
+bpy.ops.object.mode_set(mode = 'OBJECT')
+bpy.ops.object.select_all(action='DESELECT')
 bpy.ops.object.select_all(action='SELECT')
+print(len(bpy.context.selected_objects))
 for obj in bpy.context.selected_objects:
   if Muscle in obj.name and "boundary" in obj.name:
     print(obj)
     if "origin" in obj.name:
+      originCounts = reorder_coords(obj)
       origin_boundary_obj = obj
-      originCount = reorder_coords(origin_boundary_obj)
-      print("reordering origin")
+      print("reordering origin") #doesn't run this again
     if "insertion" in obj.name:
+      insertionCounts = reorder_coords(obj)
       insertion_boundary_obj = obj
-      insertionCount = reorder_coords(insertion_boundary_obj)
       print("reordering insertion")
 
-change_vertex_number(originCount,insertionCount,origin_boundary_obj,insertion_boundary_obj)
-
-
+change_vertex_number(originCounts,insertionCounts,origin_boundary_obj,insertion_boundary_obj)
