b720a0da6a6f5a8389423bd04c23bedb176fcf35
Eva C. Herbst <evacherbst@gmail.com>
2021-07-26 18:05:10 +0200

    Update test_new_joint_funct.py
    working version!

--- AddonFolder/test_new_joint_funct.py
+++ AddonFolder/test_new_joint_funct.py
@@ -54,10 +54,8 @@ def get_volume_perimeter(Muscle,n):
     # Find the closest n points to the 3d cursor
     print("Closest n points")
     for (co, index, dist) in kd.find_n(co_find, n): 
-        print(index)
+        #print(index)
         vertices_loop.append(index)
-    return(vertices_loop)
-    #print(vertices_loop)
     bpy.ops.object.mode_set(mode = 'OBJECT')
     bpy.data.objects[Muscle + " origin_merge_with_volume"].select_set(True)
     bpy.ops.object.join()
@@ -80,7 +78,6 @@ def get_volume_perimeter(Muscle,n):
     #bridge edge loops
     bpy.ops.mesh.bridge_edge_loops()
 
-
 vertices_loop = []
 Muscle = "Muscle"
 n = 32 #later as function have n be an input of number of vertices in origin boundary (maybe with loop and length, or extract from renumber vertices function)
@@ -90,28 +87,6 @@ get_volume_perimeter(Muscle,n)
 
 
 
-
-bpy.ops.object.mode_set(mode = 'OBJECT')
-bpy.data.objects[Muscle + " origin_merge_with_volume"].select_set(True)
-bpy.ops.object.join()
-muscle_volume = bpy.context.view_layer.objects.active
-muscle_volume.name = Muscle + " volume"
-bpy.ops.object.mode_set(mode = 'EDIT')
-obj = bpy.context.edit_object
-me = obj.data
-bm = bmesh.from_edit_mesh(me)
-bpy.context.tool_settings.mesh_select_mode = (False, True, False) #edgeselect mode
-bpy.ops.mesh.select_loose() #select origin boundary loop
-bpy.context.tool_settings.mesh_select_mode = (True, False, False) #vertex select mode
-for v in bm.verts:
-    if v.select:
-        vertices_loop.append(v.index)
-bpy.ops.object.mode_set(mode = 'OBJECT') #now use this list to select all boundary loops that need to be bridged
-for i in vertices_loop:
-    obj.data.vertices[i].select = True
-bpy.ops.object.mode_set(mode = 'EDIT')
-#bridge edge loops
-bpy.ops.mesh.bridge_edge_loops()
 
 
 
@@ -123,6 +98,7 @@ bpy.ops.mesh.bridge_edge_loops()
 
 
 #then remove duplicates function to get rid of edge duplicates
+bpy.ops.mesh.remove_doubles()
 
 
 #then parent?
@@ -130,143 +106,3 @@ bpy.ops.mesh.bridge_edge_loops()
 
 
 
-
-bpy.ops.object.mode_set(mode = 'OBJECT')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#with edge selected
-bpy.ops.mesh.loop_multi_select(ring=False)
-
-
-#if you join objects edge indices change, but can select edge loop and then merge and selection will still be there
-
-
-#planning
-
-#get closest edge on muscle volume to origin loop
-
-
-#on origin loop copy, set origin to geometry, set 3D cursor here
-#[select origin loop copy]
-bpy.ops.object.origin_set(type='ORIGIN_GEOMETRY', center='MEDIAN')
-bpy.ops.view3d.snap_cursor_to_selected()
-
-
-
-import mathutils
-
-# create a kd-tree from a mesh
-from bpy import context
-obj = context.object
-
-mesh = obj.data
-size = len(mesh.vertices)
-kd = mathutils.kdtree.KDTree(size)
-
-for i, v in enumerate(mesh.vertices):
-    kd.insert(v.co, i)
-
-kd.balance()
-
-
-# 3d cursor relative to the object data
-co_find = obj.matrix_world.inverted() @ context.scene.cursor.location
-
-# Find the closest n points to the 3d cursor
-print("Closest n points")
-for (co, index, dist) in kd.find_n(co_find, 2): #trying n = 2
-    print("    ", co, index, dist)
-
-
-
-
-#unselect all, then select that edge
-
-
-#select edge loop connected to that edge
-bpy.ops.mesh.loop_multi_select(ring=False)
-
-#join to origin loop
-
-
-
-
-#get closest edge on muscle volume to insertion loop
-
-
-#unselect all, then select that edge
-
-
-#select edge loop connected to that edge
-bpy.ops.mesh.loop_multi_select(ring=False)
-
-#join to insertion loop
-
-
-
-
-
-
-
-#then also make copies of origin and insertion areas and join
-
-#then merge by distance (but this could maybe cause issues if other people's resolutions are high/each units are a small increment)
-#maybe just make it really really small distance
-#select all (works on either faces or edges)
-bpy.ops.mesh.remove_doubles()
-
-
-
-import bpy, bmesh
-
-me = bpy.context.object.data
-
-bm = bmesh.new()
-bm.from_mesh(me)
-
-me.vertices[0].select = True #works, in future replace 0 with index from kd tree code above 
-
-for edge in bm.edges:
-    if edge.verts[0].select = True
-        edge.select = True
-
-bm.to_mesh(me)
-bm.free()
-
-vert[0].select_set(True)
-for edge in bm.edges:
-    if edge.verts[0].select:
-        edge.select_set(True) #THIS DOES NOT WORK, SINCE ALSO THE NON EDGE LOOP EDGE WOULD BE SELECTED. SO INSTRAD TRY TO GET EDGE LOOP FROM VERTICES
-
-#EDGE LOOP FROM VERTICES ONLY WORKS IF THEY ARE 2 ADJACENT VERTICES
-# NEW PLAN: QUERY NUMBER OF VERTICES IN ORIGIN LOOP AND SELECT THAT MANY CLOSEST ONES, SO QUERY N NUMBER OF CLOSEST POINT AND JUST SELECT
-
-
-
-
-bpy.ops.mesh.loop_multi_select(ring=True)
