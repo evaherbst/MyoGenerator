f5923d9e5673edd3c6cf883091b1b9af1e975b9c
Eva C. Herbst <evacherbst@gmail.com>
2021-07-27 14:20:16 +0200

    Update test_new_join_funct.py
    joining with origin and attachment faces

--- test_new_join_funct.py
+++ test_new_join_funct.py
@@ -20,6 +20,25 @@ def duplicate_boundaries(Muscle):
     #select items to join
     bpy.ops.object.select_all(action='DESELECT')
 
+def duplicate_attachment_areas(Muscle):
+    bpy.ops.object.select_all(action='DESELECT')
+    bpy.context.view_layer.objects.active = bpy.data.objects[Muscle + " origin"] #make active 
+    bpy.data.objects[Muscle + " origin"].select_set(True)
+    bpy.ops.object.duplicate_move(OBJECT_OT_duplicate={"linked":False, "mode":'TRANSLATION'}, TRANSFORM_OT_translate={"value":(0, 0, 0), "orient_type":'GLOBAL', "orient_matrix":((1, 0, 0), (0, 1, 0), (0, 0, 1)), "orient_matrix_type":'GLOBAL', "constraint_axis":(True, True, True), "mirror":True, "use_proportional_edit":False, "proportional_edit_falloff":'SMOOTH', "proportional_size":1, "use_proportional_connected":False, "use_proportional_projected":False, "snap":False, "snap_target":'CLOSEST', "snap_point":(0, 0, 0), "snap_align":False, "snap_normal":(0, 0, 0), "gpencil_strokes":False, "cursor_transform":False, "texture_space":False, "remove_on_cancel":False, "release_confirm":False, "use_accurate":False, "use_automerge_and_split":False})
+    bpy.context.view_layer.objects.active.name = str(Muscle + " origin_area_merge_with_volume")
+    bpy.data.objects[Muscle + " origin_area_merge_with_volume"].select_set(True)
+    bpy.ops.object.parent_clear(type='CLEAR_KEEP_TRANSFORM')
+    bpy.ops.object.select_all(action='DESELECT')
+    bpy.context.view_layer.objects.active = bpy.data.objects[Muscle + " insertion"] #make active 
+    bpy.data.objects[Muscle + " insertion"].select_set(True)
+    bpy.ops.object.duplicate_move(OBJECT_OT_duplicate={"linked":False, "mode":'TRANSLATION'}, TRANSFORM_OT_translate={"value":(0, 0, 0), "orient_type":'GLOBAL', "orient_matrix":((1, 0, 0), (0, 1, 0), (0, 0, 1)), "orient_matrix_type":'GLOBAL', "constraint_axis":(True, True, True), "mirror":True, "use_proportional_edit":False, "proportional_edit_falloff":'SMOOTH', "proportional_size":1, "use_proportional_connected":False, "use_proportional_projected":False, "snap":False, "snap_target":'CLOSEST', "snap_point":(0, 0, 0), "snap_align":False, "snap_normal":(0, 0, 0), "gpencil_strokes":False, "cursor_transform":False, "texture_space":False, "remove_on_cancel":False, "release_confirm":False, "use_accurate":False, "use_automerge_and_split":False})
+    bpy.context.view_layer.objects.active.name = str(Muscle + " insertion_area_merge_with_volume")
+    bpy.data.objects[Muscle + " insertion_area_merge_with_volume"].select_set(True)
+    bpy.ops.object.parent_clear(type='CLEAR_KEEP_TRANSFORM')
+    #select items to join
+    bpy.ops.object.select_all(action='DESELECT')
+
+
 #get edge loop by selecting closest n vertices on muscle volume to origin loop, where n = number of vertices in origin loop
 def get_volume_perimeter(Muscle,index,n):
     global vertices_loop
@@ -99,43 +118,17 @@ bpy.data.objects[Muscle].select_set(True)
 bpy.ops.object.parent_set(keep_transform=True)
 bpy.data.objects[Muscle].select_set(False) #make sure only origin is selected
 bpy.context.view_layer.objects.active = bpy.data.objects[Muscle + " volume"]
-
-
-
-
-
-def duplicate_attachment_areas(Muscle):
-    bpy.ops.object.select_all(action='DESELECT')
-    bpy.context.view_layer.objects.active = bpy.data.objects[Muscle + " origin"] #make active 
-    bpy.data.objects[Muscle + " origin"].select_set(True)
-    bpy.ops.object.duplicate_move(OBJECT_OT_duplicate={"linked":False, "mode":'TRANSLATION'}, TRANSFORM_OT_translate={"value":(0, 0, 0), "orient_type":'GLOBAL', "orient_matrix":((1, 0, 0), (0, 1, 0), (0, 0, 1)), "orient_matrix_type":'GLOBAL', "constraint_axis":(True, True, True), "mirror":True, "use_proportional_edit":False, "proportional_edit_falloff":'SMOOTH', "proportional_size":1, "use_proportional_connected":False, "use_proportional_projected":False, "snap":False, "snap_target":'CLOSEST', "snap_point":(0, 0, 0), "snap_align":False, "snap_normal":(0, 0, 0), "gpencil_strokes":False, "cursor_transform":False, "texture_space":False, "remove_on_cancel":False, "release_confirm":False, "use_accurate":False, "use_automerge_and_split":False})
-    bpy.context.view_layer.objects.active.name = str(Muscle + " origin_area_merge_with_volume")
-    bpy.data.objects[Muscle + " origin_area_merge_with_volume"].select_set(True)
-    bpy.ops.object.parent_clear(type='CLEAR_KEEP_TRANSFORM')
-    bpy.ops.object.select_all(action='DESELECT')
-    bpy.context.view_layer.objects.active = bpy.data.objects[Muscle + " insertion"] #make active 
-    bpy.data.objects[Muscle + " insertion"].select_set(True)
-    bpy.ops.object.duplicate_move(OBJECT_OT_duplicate={"linked":False, "mode":'TRANSLATION'}, TRANSFORM_OT_translate={"value":(0, 0, 0), "orient_type":'GLOBAL', "orient_matrix":((1, 0, 0), (0, 1, 0), (0, 0, 1)), "orient_matrix_type":'GLOBAL', "constraint_axis":(True, True, True), "mirror":True, "use_proportional_edit":False, "proportional_edit_falloff":'SMOOTH', "proportional_size":1, "use_proportional_connected":False, "use_proportional_projected":False, "snap":False, "snap_target":'CLOSEST', "snap_point":(0, 0, 0), "snap_align":False, "snap_normal":(0, 0, 0), "gpencil_strokes":False, "cursor_transform":False, "texture_space":False, "remove_on_cancel":False, "release_confirm":False, "use_accurate":False, "use_automerge_and_split":False})
-    bpy.context.view_layer.objects.active.name = str(Muscle + " insertion_area_merge_with_volume")
-    bpy.data.objects[Muscle + " insertion_area_merge_with_volume"].select_set(True)
-    bpy.ops.object.parent_clear(type='CLEAR_KEEP_TRANSFORM')
-    #select items to join
-    bpy.ops.object.select_all(action='DESELECT')
-
-
-
 #then duplicate origin and insertions (with faces) and merge
 duplicate_attachment_areas()
-
-#then remove duplicates function to get rid of edge duplicates
+bpy.ops.object.select_all(action='DESELECT')
+bpy.data.objects[str(Muscle + " volume")].select_set(True)
+bpy.context.view_layer.objects.active = bpy.data.objects[Muscle + " volume"] #make active
+bpy.data.objects[str(Muscle + " origin_area_merge_with_volume")].select_set(True)
+bpy.data.objects[str(Muscle + " insertion_area_merge_with_volume")].select_set(True)
+bpy.ops.object.join()
+#get rid of edge duplicates
 bpy.ops.mesh.remove_doubles()
-
-
-#then parent?
-
-
 #then triangulate mesh
-
 bpy.ops.mesh.quads_convert_to_tris(quad_method='BEAUTY', ngon_method='BEAUTY')
 
 
